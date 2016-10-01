c713dd98371f35ef51e4c7b2684f3b3e05209ebd
Ronnie Smith <rosmith@thoughtworks.com>
2016-09-30 22:23:43 -0500

    First commit

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by ronnie on 9/30/16.
+ */
+public class GameTest {
+    @Test
+    public void shouldDrawBoardWhenGameStarts() throws Exception {
+
+
+    }
+}
